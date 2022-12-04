3348c29698a5dcc6f690dff3bf80d57478ce31eb
Oldrich Pecak (LoaDy) <LoaDy588@seznam.cz>
2022-12-04 12:15:24 +0100

    add template

---
+++ template.py
@@ -0,0 +1,15 @@
+import sys
+
+
+def part_one(data):
+    return 0
+
+
+def part_two(data):
+    return 0
+
+
+with open(sys.argv[1]) as input_file:
+    input_data = input_file.readlines()
+    print(part_one(input_data))
+    print(part_two(input_data))
